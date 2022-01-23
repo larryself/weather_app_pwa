57593bcec5330b871201ccb822f186c31e558ef0
Yan Musikhin <yanmusikhin@gmail.com>
2022-01-23 14:14:13 +0400

    init: start project weatherCheck

---
+++ src/App.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const App = () => {
+  return (
+      <h1>Weather Check</h1>
+  );
+}
+
+export default App;
---
+++ src/constants/constants.js
@@ -0,0 +1,2 @@
+export const URL = 'https://api.openweathermap.org/data/2.5/weather';
+export const API_KEY = 'da6e6349b8ae1fcd53f37d263bb5998b'
