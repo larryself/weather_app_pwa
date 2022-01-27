1dad70a244ad3faf535685a18d5ac3a8a1f1064b
Yan Musikhin <yanmusikhin@gmail.com>
2022-01-27 17:21:44 +0400

    add className for link

--- src/components/cityCards/cityCards.js
+++ src/components/cityCards/cityCards.js
@@ -8,7 +8,7 @@ const CityCards = ({cities}) => {
         <ul className={'city-card__list'}>
             {cities.map(city => (
                 <li key={city.name} className={'city-card__item'}>
-                    <Link to={`/city?name=${city.name}`}>
+                    <Link to={`/city?name=${city.name}`} className={'city-card__item-link'}>
                         <CityCard city={city}/>
                     </Link>
                 </li>))
