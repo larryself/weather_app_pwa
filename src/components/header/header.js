99eb8afea7eacf9313358729328866d8ce3efe22
Yan Musikhin <yanmusikhin@gmail.com>
2022-01-27 16:59:30 +0400

    fix add link in header logo

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -1,13 +1,14 @@
 import React from 'react';
+import {Link} from 'react-router-dom';
 import './header.css';
 
 const Header = () => {
     return (
         <header className={'page-header'}>
-            <div className={'page-header__wrapper'}>
-                <img src={'img/logo.png'} alt={'logo'} width={'30'} height={'30'}/>
-                <h1 className={'page-header__title'}>WeatherCheck</h1>
-            </div>
+                <Link to={'/'}  className={'page-header__link'}>
+                    <img src={'img/logo.png'} alt={'logo'} width={'30'} height={'30'}/>
+                    <h1 className={'page-header__title'}>WeatherCheck</h1>
+                </Link>
         </header>
     );
 };
