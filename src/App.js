92eaf2b439f743ae6d2d15a16e483903315ee7d9
Yan Musikhin <yanmusikhin@gmail.com>
2022-01-27 17:20:16 +0400

    change route

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
 import React from 'react';
-import AppRouter from "./components/AppRouter/AppRouter";
+import {BrowserRouter, Routes, Route, Navigate} from "react-router-dom";
+import Index from "./pages/index";
+import City from "./pages/city/city";
+import Error from "./pages/error/error";
 import "./styles/index.css"
 
 const App = () => {
   return (
-      <AppRouter/>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Index/>}/>
+          <Route path="/city" element={<City/>}/>
+          <Route path="/404" element={<Error/>}/>
+          <Route path='*' element={<Navigate to='/404'/>}/>
+        </Routes>
+      </BrowserRouter>
   );
 }
 
