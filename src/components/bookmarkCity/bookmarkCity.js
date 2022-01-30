d13bafe49a12f821f29dabe72890079af302bfa2
Yan Musikhin <yanmusikhin@gmail.com>
2022-01-30 21:40:28 +0400

    fix : add components bookmarkCity

--- src/components/bookmarkCity/bookmarkCity.js
+++ src/components/bookmarkCity/bookmarkCity.js
@@ -1,6 +1,7 @@
 import React,{useState,useEffect} from 'react';
 import { LOCAL_STORAGE_KEY } from '../../constants/constants';
 import Icon from '../../components/icon/icon';
+import './bookmarkCity.css';
 
 const BookmarkCity = ({cityName}) => {
   const [bookmark, setBookmark] = useState(false);
@@ -22,8 +23,8 @@ const BookmarkCity = ({cityName}) => {
     }
   }, [cityName]);
   return (
-      <button type='button' className='city__bookmark' onClick={handleBookmark}>
-        <Icon width={24} height={24} className='city__bookmark-icon'
+      <button type='button' className='city__bookmark bookmark' onClick={handleBookmark}>
+        <Icon width={24} height={24} className='bookmar__icon'
               name={bookmark ? 'bookmark_use' : 'bookmark'} />
       </button>
   );
