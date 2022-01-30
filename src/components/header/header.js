b0738fb7452c3f88431498d0ccc048d5f7742233
Yan Musikhin <yanmusikhin@gmail.com>
2022-01-30 10:45:43 +0400

    fix add img srcSet for retina

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -5,7 +5,7 @@ import './header.css';
 const Header = () => (
   <header className='page-header'>
     <Link to='/' className='page-header__link'>
-      <img src='img/logo.png' alt='logo' width='30' height='30' />
+      <img src='img/logo.png' srcSet='img/logo@2.png 2x' alt='logo' width='30' height='30' />
       <h1 className='page-header__title'>WeatherCheck</h1>
     </Link>
   </header>
